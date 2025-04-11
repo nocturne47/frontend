2056339ef8d770010c6cf7eea463f18943e325ff
nocturne47 <immanuelnaibaho1@gmail.com>
2025-04-11 23:10:02 +0700

    coba ubah tanpa public

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -92,7 +92,7 @@ function Projects() {
                   >
                     {playing[index] ? (
                       <video
-                        src={`public/assets/proj/${row.video}`} // Replace with the actual video URL if different per project
+                        src={`/assets/proj/${row.video}`} // Replace with the actual video URL if different per project
                         controls
                         autoPlay
                         className="media-element cursor-pointer"
@@ -102,7 +102,7 @@ function Projects() {
                       />
                     ) : (
                       <img
-                        src={`public/assets/proj/${row.gambar}`}
+                        src={`/assets/proj/${row.gambar}`}
                         alt={row.judul}
                         className="media-element cursor-pointer"
                         onClick={() => handleToggle(index)}
